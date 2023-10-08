20cfb82f2c13630ae0937cc7a960296a323dda81
tm alone <tmalone@protonmail.ch>
2023-10-08 18:20:25 -0400

    Added Logger config to the package __init__.py file

--- websnap/__init__.py
+++ websnap/__init__.py
@@ -3,3 +3,6 @@ WebSnap
 
 Snapshots of any web-based image, page section, or entire webpage: anything which can be displayed on a single screen.
 """
+import logging
+
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
